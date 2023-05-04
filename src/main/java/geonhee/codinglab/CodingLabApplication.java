fc0d458b6c69888c9422a7b4964292977ba3428f
geony <jvckigh@gmail.com>
2023-05-04 16:33:05 +0900

    Init project

---
+++ src/main/java/geonhee/codinglab/CodingLabApplication.java
@@ -0,0 +1,13 @@
+package geonhee.codinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingLabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodingLabApplication.class, args);
+  }
+
+}
---
+++ src/test/java/geonhee/codinglab/CodingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package geonhee.codinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingLabApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
